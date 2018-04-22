8b9f7ba1366177b7332a2d1046b748ca8020158d
mathias_oscarsson <mathiasoscarsson@gmail.com>
2018-04-22 16:10:12 +0200

    Removed duplicate line

--- config.rb
+++ config.rb
@@ -1,20 +1,13 @@
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
 activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
 
-activate :relative_assets
 activate :livereload
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
-
-# Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
@@ -22,39 +15,10 @@ page '/*.txt', layout: false
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
 
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 require 'extensions/build_cleaner'
 
 configure :build do
   activate :relative_assets
   activate :build_cleaner
 end
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
